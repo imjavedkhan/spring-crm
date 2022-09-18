127b8208a0b3107dc8f02fbbb7cb53b3ab9e7488
Javed Khan <javedalikhan50@gmail.com>
2022-09-18 21:40:29 +0530

    Added Frontend feature to App

--- src/main/java/com/primus/springcrm/controller/StudentController.java
+++ src/main/java/com/primus/springcrm/controller/StudentController.java
@@ -3,13 +3,13 @@ package com.primus.springcrm.controller;
 import com.primus.springcrm.entity.Student;
 import com.primus.springcrm.services.StudentService;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import javax.annotation.PostConstruct;
 import java.util.List;
 
-@RestController
-@RequestMapping("/")
+@Controller
 public class StudentController {
 
     private final StudentService studentService;
@@ -18,27 +18,43 @@ public class StudentController {
         this.studentService = studentService;
     }
 
-    @GetMapping("students")
-    public List<Student> getAllStudents(){
-        return studentService.getAllStudents();
+    @GetMapping("/")
+    public String getAllStudents(Model model){
+
+       List<Student> thestudents =  studentService.getAllStudents();
+
+        model.addAttribute("students", thestudents);
+
+        return  "index";
     }
 
-    @GetMapping("student/{id}")
-    public Student getStudent(@PathVariable Long id){
-        return studentService.getStudent(id);
+    @PostMapping("/showFormForUpdate")
+    public String getStudent(@RequestParam("studentId") Long theId,
+                              Model theModel){
+        Student theStudent =  studentService.getStudent(theId);
+
+        theModel.addAttribute("new_student",theStudent);
+
+        return "add_student";
     }
 
-    @DeleteMapping("student/delete/{id}")
-    public String deleteStudent(@PathVariable Long id){
+    @PostMapping("student/delete")
+    public String deleteStudent(@RequestParam("studentId") Long id){
          studentService.deleteStudent(id);
-        return "Student deleted";
+        return "redirect:/";
     }
 
     @PostMapping("save/student")
-    public String saveStudent(@RequestBody Student student){
+    public String saveStudent(@ModelAttribute("new_student") Student student){
         studentService.saveStudent(student);
-        return "Student saved";
+        return "redirect:/";
     }
 
+    @GetMapping("/new")
+    public String showFormForStudent(Model themodel){
+        Student student = new Student();
+        themodel.addAttribute("new_student",student);
+        return "add_student";
+    }
 
 }
